651a4b229094d2b34a121e28db94e64d9cae7ab4
pelx <laura.peppiatt@btinternet.com>
2019-04-08 23:00:24 +0100

    Second

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -17,26 +17,44 @@ export class ItemComponent implements OnInit {
 	form: FormGroup;
 
 	gender: Gender[] = [ { code: 'F', value: 'Female' }, { code: 'M', value: 'Male' } ];
-	constructor(private fb: FormBuilder, private http: HttpClient, private route: ActivatedRoute) {
+	constructor(private http: HttpClient, private route: ActivatedRoute) {
 		this.id = +this.route.snapshot.paramMap.get('id');
 		this.http.get<Item[]>('http://127.0.0.1:8887/mock-data.json').subscribe((data) => {
 			let items = data as Array<Item>;
 
 			this.item = items.find((i) => i.id === this.id);
 
-			console.log('ITEM', this.item);
-			console.log('ITEM', this.item.last_name);
-
-			this.form = fb.group({
-				id: [ this.id ],
-				first_name: [ this.item.first_name ],
-				last_name: [ this.item.last_name ],
-				gender: [ this.item.gender ],
-				email: [ this.item.email ],
-				colour: [ this.item.colour ]
-			});
+			this.initForm(this.item);
 		});
 	}
 
 	ngOnInit() {}
+
+	initForm(item: Item) {
+		console.log('ITEM', item);
+		console.log('ITEM', item.last_name);
+
+		let fb = new FormBuilder();
+
+		this.form = fb.group({
+			id: [ item.id ],
+			first_name: [ item.first_name ],
+			last_name: [ item.last_name ],
+			gender: [ item.gender ],
+			email: [ item.email ],
+			colour: [ item.colour ]
+		});
+	}
 }
+
+// console.log('ITEM', this.item);
+// 			console.log('ITEM', this.item.last_name);
+
+// 			this.form = fb.group({
+// 				id: [ this.id ],
+// 				first_name: [ this.item.first_name ],
+// 				last_name: [ this.item.last_name ],
+// 				gender: [ this.item.gender ],
+// 				email: [ this.item.email ],
+// 				colour: [ this.item.colour ]
+// 			});
